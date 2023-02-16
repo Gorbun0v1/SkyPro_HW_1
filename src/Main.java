845502990e9ea79a8720e2475e97897518729800
Проверка <joongleboo01@gmail.com>
2023-02-16 20:19:00 +0300

    Повтор знаний, пуш в гит

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Привет");
-        System.out.println("Повтор!!!");
+        var dog = "Собака, привет!";
+        System.out.println(dog);
     }
 }
